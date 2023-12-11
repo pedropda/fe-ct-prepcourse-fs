7560cd42a50de485a0a20cfaf23bcfa3910450ab
pedropda <pedropda@icloud.com>
2023-12-10 21:39:06 -0600

    10122023

--- M05 Fundamentos Javascript/ejercicios/10.js
+++ M05 Fundamentos Javascript/ejercicios/10.js
@@ -3,6 +3,8 @@ function esFechaValida(fecha) {
   // Comprueba si este corresponde a una fecha válida.
   // Si es así, retorna true, sino retorna false.
   // Tu código:
+  const pruebaFecha = new Date(fecha);
+  return !isNaN(pruebaFecha.getTime());
 }
 
 module.exports = esFechaValida;
